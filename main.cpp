6946cfd53aab63ed372833f3bd932521f07d8349
Avtobu <lifeivanna2007@gmail.com>
2026-02-12 18:35:23 +0200

    Added main logic, added base logic of passwording id

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include "Student.h"
+#include "Person.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {}
+int main() {
+    int id;
+    std::cin >> id;
+    Person st("G", 18, id);
+    st.printInfo();
+    std::cout << "Enter password: "<< std::endl;
+    int password = 1234;
+    int pass;
+    std::cin >> pass;
+    if (pass == password) {
+        st.showid();
+    }
+    else{std::cout << "Not correct password"; return 1;}
+
+}
